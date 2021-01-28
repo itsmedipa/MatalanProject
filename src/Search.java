4e0622555194a635f6dbf9f96955444c55a8c0ed
Ravinder <ravinder.gugloth@gmail.com>
2021-01-28 20:31:19 +0000

    Changes done

--- src/Search.java
+++ src/Search.java
@@ -3,12 +3,13 @@ public class Search {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is my first GIT");
-int a,b,c;
-a=20;
-b=45;
-c=a+b;
-System.out.println(c);
+		System.out.println("This is my first GIT");
+		int a, b, c;
+		a = 20;
+		b = 45;
+		c = a + b;
+		System.out.println(c);
+		System.out.println("Addition of two numbers");
 	}
 
 }
